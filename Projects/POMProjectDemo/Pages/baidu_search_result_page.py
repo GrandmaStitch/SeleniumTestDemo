d7c94e9ae642e1520bdf15b6d1fd46e9e2cc00b6
Apple <456789hahahaha@sina.com>
2021-02-26 11:19:25 +0800

    use Locators class

--- Projects/POMProjectDemo/Pages/baidu_search_result_page.py
+++ Projects/POMProjectDemo/Pages/baidu_search_result_page.py
@@ -1,8 +1,10 @@
+from Projects.POMProjectDemo.Locators.locators import Locators
+
 class SearchResultPage():
 
     def __init__(self, driver):
         self.driver = driver
-        self.openWebsite_linkText = 'Apple (中国) - 官方网站'
+        self.openWebsite_linkText = Locators.openWebsite_linkText
 
     def click_link(self):
         self.driver.find_element_by_link_text(self.openWebsite_linkText).click()
