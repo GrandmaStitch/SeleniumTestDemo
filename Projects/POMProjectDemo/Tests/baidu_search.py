050331cb3ff76cc9ac054a35ef0f1e051db25341
Apple <456789hahahaha@sina.com>
2021-02-26 11:43:14 +0800

    add statement for running from command line

--- Projects/POMProjectDemo/Tests/baidu_search.py
+++ Projects/POMProjectDemo/Tests/baidu_search.py
@@ -2,7 +2,9 @@ from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-import os, unittest,time
+import os, sys, unittest,time
+# run from command line setting
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..', '..'))
 from Projects.POMProjectDemo.Pages.baidu_search_page import SearchPage
 from Projects.POMProjectDemo.Pages.baidu_search_result_page import SearchResultPage
 
