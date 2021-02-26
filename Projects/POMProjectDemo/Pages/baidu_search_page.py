a9c64c94a54c352b0e53194f1d0297a326ac5691
Apple <456789hahahaha@sina.com>
2021-02-26 11:18:52 +0800

    use Locators class

--- Projects/POMProjectDemo/Pages/baidu_search_page.py
+++ Projects/POMProjectDemo/Pages/baidu_search_page.py
@@ -1,10 +1,12 @@
+from Projects.POMProjectDemo.Locators.locators import Locators
+
 class SearchPage():
 
     def __init__(self, driver):
         self.driver = driver
 
-        self.searchInput_textbox_name = 'wd'
-        self.search_button_id = 'su'
+        self.searchInput_textbox_name = Locators.searchInput_textbox_name
+        self.search_button_id = Locators.search_button_id
 
     def enter_search_keyword(self, keyword):
         self.driver.find_element_by_name(self.searchInput_textbox_name).clear()
