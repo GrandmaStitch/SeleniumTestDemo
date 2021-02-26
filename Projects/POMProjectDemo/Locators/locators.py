67ce5b29db8ffb5a9a1c379ad54c8808dafc4ad3
Apple <456789hahahaha@sina.com>
2021-02-26 11:17:20 +0800

    create a seperate class for Locators

---
+++ Projects/POMProjectDemo/Locators/locators.py
@@ -0,0 +1,8 @@
+class Locators():
+
+    # Search page objects
+    searchInput_textbox_name = 'wd'
+    search_button_id = 'su'
+
+    # Search result page objects
+    openWebsite_linkText = 'Apple (中国) - 官方网站'
