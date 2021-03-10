e59da8754ecd2597201c922dd082bf4793da94ef
Apple <456789hahahaha@sina.com>
2021-03-10 10:16:59 +0800

    refactor code

--- Projects/POMProjectDemo/Tests/baidu_search.py
+++ Projects/POMProjectDemo/Tests/baidu_search.py
@@ -2,11 +2,12 @@ from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-import os, sys, unittest,time
+import os, sys, unittest,time, shutil
 # run from command line setting
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..', '..'))
 from Projects.POMProjectDemo.Pages.baidu_search_page import SearchPage
 from Projects.POMProjectDemo.Pages.baidu_search_result_page import SearchResultPage
+from TestRunner import HTMLTestRunner
 
 
 class SearchTest(unittest.TestCase):
@@ -14,7 +15,7 @@ class SearchTest(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         root_path = os.path.abspath(os.path.dirname(os.getcwd()) + os.path.sep + '..' + os.path.sep + '..')
-        driver_type = 'chromedriver88.exe'
+        driver_type = 'chromedriver89.exe'
         driver_path = os.path.abspath(root_path + os.path.sep + 'drivers' + os.path.sep + driver_type)
 
         cls.driver = webdriver.Chrome(driver_path)
@@ -41,42 +42,36 @@ class SearchTest(unittest.TestCase):
         search_result_page.click_link()
         self.driver.implicitly_wait(10)
 
+        time.sleep(10)
+
     @classmethod
     def tearDownClass(cls):
         cls.driver.close()
-        # cls.driver.quit()
+        try:
+            cls.driver.quit()
+        except Exception as e:
+            print(e)
         print('Test completed!')
+
 
 if __name__ == '__main__':
-    unittest.main()
+    # unittest.main()
+    # testRunner = HtmlTestRunner.HTMLTestRunner(output='D:/TryTryTry/SeleniumTestDemo/Reports')
+    suit = unittest.TestSuite()
+    suit.addTest(SearchTest("test_search"))
+    now = time.strftime('%Y%m%d%H%M%S')
+    with(open(f'Test_Report_{now}.html', 'wb')) as fp:
+        runner = HTMLTestRunner(
+            stream=fp,
+            title='测试报告',
+            description='测试用例demo'
+        )
+        runner.run(suit)
 
+    source_path = os.getcwd()
+    target_path = os.path.abspath(
+        os.path.dirname(os.getcwd()) + os.path.sep + '..' + os.path.sep + '..' + os.path.sep + 'Reports')
+    for f in os.listdir(source_path):
+        if f.endswith('.html'):
+            shutil.move(f, target_path)
 
-
-
-'''
-# 搜索页
-try:
-    driver.get(url)
-
-    driver.implicitly_wait(10)
-
-    if driver.find_element_by_link_text('百度热榜'):
-
-        driver.find_element_by_name('wd').send_keys(keyword)
-
-        driver.find_element_by_id('su').click()
-
-        driver.implicitly_wait(10)
-
-        # 搜索结果页
-        driver.find_element_by_link_text('Apple (中国) - 官方网站').click()
-
-        driver.implicitly_wait(10)
-
-        print('Test completed!')
-
-except:
-    print("Can't open page!")
-    driver.close()
-    driver.quit()
-'''
