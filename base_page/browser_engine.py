702f7600504822f345b3d53cf4dadfd378613cfd
sui <grandmasunshinessui@github.com>
2021-05-24 00:25:34 +0800

    initial commit

---
+++ base_page/browser_engine.py
@@ -0,0 +1,33 @@
+import configparser
+import os
+
+from selenium import webdriver
+
+
+class BrowserEngine(object):
+    def __init__(self, browser=None):
+        self.driver_path = os.path.abspath(
+            os.path.dirname(os.getcwd()) + os.path.sep + 'drivers'+ os.path.sep + 'chromedriver_mac_89')
+        file_path = os.path.abspath(os.path.dirname(
+            os.path.dirname(__file__))) + '/config.ini'
+        config = configparser.ConfigParser()
+        config.read(file_path, encoding='utf-8')
+        if browser is None:
+            self._browser_type = config.get('browserType', 'browserName')
+        else:
+            self._browser_type = browser
+        self._driver = None
+
+    def init_driver(self):
+        if self._browser_type.lower() == 'chrome':
+            self._driver = webdriver.Chrome(self.driver_path)
+        elif self._browser_type.lower() == 'firefox':
+            self._driver = webdriver.Firefox()
+        elif self._browser_type.lower() == 'ie':
+            self._driver = webdriver.Ie()
+        elif self._browser_type.lower() == 'edge':
+            self._driver = webdriver.Edge()
+        else:
+            ValueError('传入的浏览器类型有误，目前仅支持Chrome/Firefox/IE/Edge浏览器')
+        self._driver.maximize_window()
+        return self._driver
