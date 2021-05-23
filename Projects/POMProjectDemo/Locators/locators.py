e5eff858f4067745d20c5cc8fc17642622f4a90a
sui <grandmasunshinessui@github.com>
2021-05-24 00:14:06 +0800

    remove older files

--- Projects/POMProjectDemo/Locators/locators.py
+++
@@ -1,8 +0,0 @@
-class Locators():
-
-    # Search page objects
-    search_textbox_name = 'wd'
-    search_button_id = 'su'
-
-    # Search result page objects
-    openWebsite_linkText = 'Apple (中国) - 官方网站'
