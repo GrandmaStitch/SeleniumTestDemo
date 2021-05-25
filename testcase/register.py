3d7c87409c2843fb5524bf2a2153d4e5f807dfe0
sui <grandmasunshinessui@github.com>
2021-05-26 00:14:19 +0800

    fetch code url

--- testcase/register.py
+++ testcase/register.py
@@ -1,5 +1,8 @@
 import time
 import unittest
+import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
 from base_page.browser_engine import BrowserEngine
 from base_page.base_page import BasePage
@@ -12,9 +15,9 @@ from selenium.webdriver.common.by import By
 
 '''
 class LoginPage(unittest.TestCase):
-    
+
     url = 'http://h5-saas-dev.myutopa.com/login?redirect=%2Findex'
-    
+
     def setUp(self):
         self.driver = BrowserEngine().init_driver()
         self.driver.get(url)
@@ -28,18 +31,20 @@ driver = BrowserEngine(browser='chrome').init_driver()
 driver.get(url)
 driver.find_element_by_link_text('去注册').click()
 '''
-driver = BrowserEngine(browser='chrome').init_driver()
+driver = BrowserEngine().init_driver()
 driver.get(url1)
 for i in range(26):
-    ll = driver.find_elements_by_xpath('//*[@class="col-xs-12 col-md-8"]')[i].get_attribute('outerHTML')
-    # ll = driver.find_elements_by_xpath('//*[@class="col-xs-12 col-md-8"]')[i].get_attribute('currentSrc')
-    if '【新航道】您的验证码是' in ll:
-        print(ll)
+    fetch_text = driver.find_elements_by_xpath('//*[@class="col-xs-12 col-md-8"]')[i].get_attribute('outerHTML')
+    if '【网易】您的验证码为' in fetch_text:
+        break
+
+print(fetch_text)
+res = fetch_text.split('"')
+print(res[5])
 
 
 # print(driver.find_element_by_link_text('【U智零售】您的验证码是'))
 
-# inputContext1 = driver.find_element_by_xpath('//input[@class="ivu-input"]').get_attribute('value')
 
-# driver.quit()
+driver.quit()
 # driver.close()
